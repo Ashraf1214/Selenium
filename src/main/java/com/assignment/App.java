3318784c711f9a4c6062cd8419f92e46c68d075a
unknown <ashrafhossain90@gmail.com>
2024-10-22 00:38:56 -0400

    Final test app

--- src/main/java/com/assignment/App.java
+++ src/main/java/com/assignment/App.java
@@ -1,14 +1,16 @@
 package com.assignment;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-//import io.github.bonigarcia.wdm.WebDriverManager;
-
+import java.io.IOException;
+import java.io.File;
 import java.time.Duration;
 
 /**
@@ -17,7 +19,8 @@ import java.time.Duration;
  */
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
+	@Test
+    public static void main( String[] args ) throws InterruptedException, IOException
     {
 
         //To run in headless mode
@@ -25,29 +28,26 @@ public class App
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
         //WebDriver driver = new EdgeDriver(options);
-        driver.get("https://www.facebook.com/");
+        driver.get("http://18.117.226.30:8081/contact.html");
         
         driver.getTitle();
         
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+        driver.findElement(By.name("your_name")).sendKeys("Ashraf");
+        driver.findElement(By.name("phone_number")).sendKeys("9999999999");
+        driver.findElement(By.name("email_address")).sendKeys("abc@tcs.com");
+        driver.findElement(By.name("your_message")).sendKeys("This is a test by Selenium");
+        driver.findElement(By.id("my-button")).click();
+        String message = driver.findElement(By.id("response")).getText();
+        System.out.println(message);
+        System.out.println("Tested Successfully");
         
-        WebElement email = driver.findElement(By.id("email"));
-        WebElement password = driver.findElement(By.id("pass"));
-        WebElement login = driver.findElement(By.name("login"));
-        Thread.sleep(2000);
-        email.sendKeys("abc@tcs.com");
-        password.sendKeys("TCS#2024");
-        login.click();
-        Thread.sleep(2000);
-        try {
-            WebElement errorMessage = driver.findElement(By.className("_9ay7"));
-            System.out.println("Error Message: " + errorMessage.getText());
-        } catch (Exception e) {
-            System.out.println("Error message not found or login successful.");
-        }
-        
-        System.out.println("The chrome versions used here is "+((ChromeDriver) driver).getCapabilities().getCapability("chrome").toString());
-
+        TakesScreenshot scrShot = ((TakesScreenshot)driver);
+        File srcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File("test-report.jpg");
+   
+		FileUtils.copyFile(srcFile,destFile);
+		
         driver.quit();
     }
 }
